2d0c4a3b114f0b5dee93aa040c8115de7c53d804
Vishal Singh <vishalkumar70854@gmail.com>
2023-09-12 19:28:19 +0530

    orm, hibernte, jpa, added tables using hibernate, jdbc intro

--- productservice/src/main/java/dev/vishal/productservice/models/Product.java
+++ productservice/src/main/java/dev/vishal/productservice/models/Product.java
@@ -1,9 +1,18 @@
 package dev.vishal.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Product extends BaseModel{
 	private String title;
     private String description;
     private String image;
+    //            P : C
+    // => L to R: 1 : 1
+    // => R to L: m : 1
+    // => Ans:    m : 1
+    @ManyToOne
     private Category category;
     private double price;
 }
